a9a73dd5a7423cf314983af47c0f2504b65dafb1
PBF4HP <164922255+PBF4HP@users.noreply.github.com>
2024-03-27 11:23:04 +0100

    Update 1.0.3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var list = {"ii":[31,31,31,31,31,31],"i":[31,31,31,31,31,31]}
-var listc = {} // COOKIES DATA
-var maps = { 
+var list = [31,31,31,31,31,31]
+var rlist = ["o","o","o","o","o","o"]
+var maps = {
     "ICE":["oeoooo","ooeooo","eooooo","eeoooo"],
     "FIRE":["oeeoeo","eeeoeo","oooeeo","eooeeo"],
     "GROUND":["oeeeoe","ooeeoe","eeoeoe","eoeeoe"],
@@ -18,164 +18,34 @@ var maps = {
     "PSYCHIC":["oeeooo","eeeooo","eoeooo","oooeoo"],
     "DARK":["eeoeee","ooeeee","eoeeee","oeeeee","eeeeee"]
 }
-function rreload(){
-    reload('typelabel','ii')
-    reload('hid2','i')
-}
-function reload(special,t){
-    var stats2 = [];
-    for (let i = 0; i < 6; i++){
-        stats2[i] = Math.round(document.getElementById(t+i).value)
-    }
-    list[t] = stats2
-    document.getElementById(special).innerHTML = getHP(list[t])
-    return getHP(list[t])
-}
-/*function stat(obj,number,t,label){
-    if(!list[t]){list[t]=[31,31,31,31,31,31]}
-    if (isNaN(obj.value)){obj.value = list[t][number]}
-    if (obj.value> 31){obj.value = list[t][number]
-    }else if(obj.value<0){obj.value = 0}
-    list[t][number] = parseInt(obj.value)
-    document.getElementById(label).innerHTML = getHP(list[t])
-    return getHP(list[t])
-}*/
-function stat(obj,number,t,label){
-    var ivs = []
-    for (let i = 0; i < 6; i++){
-        ivs[i] = Math.round(document.getElementById(t+i).value)
-    }
-    document.getElementById(label).innerHTML = getHP(ivs)
-}
-function getMAP(stats){
-    var odd = ""
-    for (let i in stats) {
-        odd = stats[i]%2==0 ? odd+"e" : odd+"o"
-    }
-    return odd
-}
-function getHP(stats){
-    var v = getMAP(stats)
+function gettype(){
+    var v = rlist[0]+rlist[1]+rlist[2]+rlist[3]+rlist[4]+rlist[5]
     for (let i in maps) {
         for (let ia in maps[i]) {
             if(v==maps[i][ia]){return i}
         }
       }
-    return "ERROR"
 }
-/* BUTTON */
-function popup(){
-    loadobjects()
-    if( document.getElementById("button").classList.contains('buttond') ){
-        document.getElementById("button").classList.remove('buttond')
-    }else{document.getElementById("button").classList.add('buttond')}
-    return 0
-}
-function saveOpen(obj){
-    if( obj.parentNode.parentNode.classList.contains('opened') ){
-        obj.parentNode.parentNode.classList.remove('opened')
-        obj.parentNode.querySelector("#minput").disabled = true
+function createlist(x){
+    if((list[x]%2)==0){
+        rlist[x] = "e"
     }else{
-        obj.parentNode.parentNode.classList.add('opened')
-        obj.parentNode.querySelector("#minput").disabled = false
+        rlist[x] = "o"
     }
-    return 0  
+    document.getElementById("typelabel").innerHTML = gettype()
 }
-function removeOpen(num){
-    removeData(num)
-    loadobjects()
-}
-function saveDataPart(name, id, value, adiv){
-    if(name == "name"){
-        listc.pokemons[id][0] = value
-    }else{
-        listc.pokemons[id][1][adiv] = value
+function stat(val,fr,num){
+    if (isNaN(val)){
+        fr.value = list[num-1]
+        val = list[num-1]
     }
-    save()
-}
-/* COOKIES FUNCIONS */
-function addData(name,ivs){
-    const listv = [name,ivs];
-    listc.pokemons.push(listv)
-    document.cookie =  "saves="+JSON.stringify(listc)+"; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT"
-    return document.cookie
-}
-function removeData(num){
-    if(listc.pokemons[num] != undefined){
-    listc.pokemons.splice(num,1)
-    document.cookie =  "saves="+JSON.stringify(listc)+"; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT"
+    if (val> 31){
+        fr.value = list[num-1];
+        val = list[num-1];
+    }else if(val<0){
+        fr.value = 0
+        val = 0
     }
-    return document.cookie
+    list[num-1] = parseInt(val)
+    createlist(num-1)
 }
-function loadData(){
-    let matches = document.cookie.match(new RegExp("(?:^|; )saves=([^;]*)"))
-    return matches ? decodeURIComponent(matches[1]) : undefined
-}
-function save(){
-    document.cookie =  "saves="+JSON.stringify(listc)+"; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT"
-    return document.cookie
-}
-/* Button for cookies */
-function create(){
-    var ivs = []
-    var name = document.getElementById("namesto").value
-    document.getElementById("namesto").value = ""
-
-    for (let i = 0; i < 6; i++){
-        ivs[i] = Math.round(document.getElementById("i"+i).value)
-    }
-    for (let i = 0; i < 6; i++){
-        document.getElementById("i"+i).value = 31
-    }
-    addData(name,ivs)
-    loadobjects()
-    reload('hid2','i')
-}
-function arrmove(type,num){
-    if(type=="up"){
-        if(num >0){
-            const element = listc.pokemons.splice(num, 1)[0];
-            listc.pokemons.splice(num-1, 0, element);
-        }
-    }else{
-        if(num < listc.pokemons.length){
-            const element = listc.pokemons.splice(num, 1)[0];
-            listc.pokemons.splice(num+1, 0, element);     
-        }
-    }
-    save()
-    loadobjects()
-}
-function loadobjects(){
-    document.getElementById("storage").innerHTML = ""
-    for(let i in listc.pokemons){
-        document.getElementById("storage").innerHTML = document.getElementById("storage").innerHTML+
-        `
-        <div class="label" id="tg`+i+`">
-        <div id="rlabel">
-            <label id="mlabel"><button id="mbutton1" onclick="arrmove('up',`+i+`)">&#11165;</button><button id="mbutton2" onclick="arrmove('down',`+i+`)">&#11167;</button></label>
-            <button id="open" onclick="saveOpen(this)">V</button>
-            <button id="remove" onclick="removeOpen(`+i+`)">X</button>
-            <input id="minput" placeholder="NAME" value="`+listc.pokemons[i][0]+`" onchange="saveDataPart('name',`+i+`,this.value,0)" disabled>
-            <br></br>
-            <input class="IV2" id="ivr`+i+`0" placeholder="IV" type="number" max=31  min="0" value="`+listc.pokemons[i][1][0]+`" onchange="saveDataPart('in',`+i+`,this.value,0)" oninput="stat(this,0,'ivr`+i+`','ivr`+i+`h')" >
-            <input class="IV2" id="ivr`+i+`1" placeholder="IV" type="number" max=31  min="0" value="`+listc.pokemons[i][1][1]+`" onchange="saveDataPart('in',`+i+`,this.value,1)" oninput="stat(this,1,'ivr`+i+`','ivr`+i+`h')" >
-            <input class="IV2" id="ivr`+i+`2" placeholder="IV" type="number" max=31  min="0" value="`+listc.pokemons[i][1][2]+`" onchange="saveDataPart('in',`+i+`,this.value,2)" oninput="stat(this,2,'ivr`+i+`','ivr`+i+`h')" >
-            <input class="IV2" id="ivr`+i+`3" placeholder="IV" type="number" max=31  min="0" value="`+listc.pokemons[i][1][3]+`" onchange="saveDataPart('in',`+i+`,this.value,3)" oninput="stat(this,3,'ivr`+i+`','ivr`+i+`h')" >
-            <input class="IV2" id="ivr`+i+`4" placeholder="IV" type="number" max=31  min="0" value="`+listc.pokemons[i][1][4]+`" onchange="saveDataPart('in',`+i+`,this.value,4)" oninput="stat(this,4,'ivr`+i+`','ivr`+i+`h')" >
-            <input class="IV2" id="ivr`+i+`5" placeholder="IV" type="number" max=31  min="0" value="`+listc.pokemons[i][1][5]+`" onchange="saveDataPart('in',`+i+`,this.value,5)" oninput="stat(this,5,'ivr`+i+`','ivr`+i+`h')" >
-            <br><br><b> <label id="hide">HIDDEN POWER: <a id="ivr`+i+`h">`+getHP(listc.pokemons[i][1])+`</a></label></b>
-        </div>
-        </div>
-        `
-    }
-}
-/*              */
-/*              */
-const Datalist = loadData()
-const newSave = {"pokemons":[["Volcarona",[31,31,30,30,31,30]],["Venusaur",[31,30,30,31,30,31]],["Amoonguss",[31,30,30,31,30,31]]]}
-listc = Datalist != undefined ? JSON.parse(Datalist) : newSave
-if(listc == {}){
-    listc = newSave
-}
-save()
